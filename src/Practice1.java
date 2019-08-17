9b2d14e4c56212340b3d8874224ea51b2b215e6d
Nancy-06 <1456387238@qq.com>
2019-08-17 16:53:47 +0800

    java输入数字打印

---
+++ src/Practice1.java
@@ -0,0 +1,10 @@
+//java输入数字打印
+import java.util.Scanner;
+public class Practice1 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String intValue=scanner.next();
+        System.out.println(intValue);
+    }
+}
+
