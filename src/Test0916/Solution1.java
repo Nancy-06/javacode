18f1cd24aca6fd061d4514b0779ecce51e234ff7
Nancy-06 <1456387238@qq.com>
2019-09-16 23:08:32 +0800

    在数组中找出三个数之和为0的不重复数组

---
+++ src/Test0916/Solution1.java
@@ -0,0 +1,128 @@
+package Test0916;
+
+import java.util.*;
+//三数之和为0
+public class Solution1 {
+    public static List<List<Integer>> threeSum1(int [] nums){
+        Arrays.sort(nums);
+        List<List<Integer>> result=new ArrayList<>();
+        int i=0;
+        while(i<nums.length-2){
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+                while(j<k&&nums[i]+nums[j]+nums[k]<0){
+                    j++;
+                }
+                if(j>=k){
+                    break;
+                }
+                if(nums[i]+nums[j]+nums[k]==0){
+                    List<Integer> list=new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    result.add(list);
+                    while(j<k&&nums[j]==nums[j+1]){
+                        j++;
+                    }
+                    j++;
+                }
+                while(j<k&&nums[i]+nums[j]+nums[k]>0){
+                    k--;
+                }
+                if(j>=k){
+                    break;
+                }
+                if(nums[i]+nums[j]+nums[k]==0){
+                    List<Integer> list=new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    result.add(list);
+                    while(j<k&&nums[k]==nums[k-1]){
+                        k--;
+                    }
+                    k--;
+                }
+            }
+            while(i<nums.length&&nums[i]==nums[i+1]){
+                i++;
+            }
+            i++;
+        }
+        return result;
+    }
+
+    public static List<List<Integer>> threeSum2(int [] nums){
+        List<List<Integer>> result=new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+                int s=nums[i]+nums[j]+nums[k];
+                if(s<0){
+                    j++;
+                }
+                else if(s>0){
+                    k--;
+                }
+                else{
+                    List<Integer> list=new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    result.add(list);
+                    while(j<k&&nums[j]==nums[j+1]){
+                        j++;
+                    }
+                    j++;
+                    while(j<k&&nums[k]==nums[k-1]){
+                        k--;
+                    }
+                    k--;
+                }
+            }
+            while(i<nums.length-2&&nums[i]==nums[i+1]){
+                i++;
+            }
+        }
+        return result;
+    }
+
+    public static List<List<Integer>> threeSum3(int [] nums){
+        HashSet<List<Integer>> result=new HashSet<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+                int s=nums[i]+nums[j]+nums[k];
+                if(s<0){
+                    j++;
+                }
+                else if(s>0){
+                    k--;
+                }
+                else{
+                    List<Integer> list=new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    result.add(list);
+                    j++;
+                    k--;
+                }
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
+    public static void main(String[] args) {
+        int [] nums={-1,-1,0,2,-4,1};
+        List<List<Integer>> r=threeSum3(nums);
+        System.out.println(r);
+        //System.out.println(threeSum2(nums));
+    }
+}
