3496bd5528d1d08eb92b1f7f252fcd37f0967c45
Nancy-06 <1456387238@qq.com>
2019-11-25 19:43:00 +0800

    百万富翁问题

---
+++ src/Test1125.java
@@ -0,0 +1,15 @@
+/**
+ * @author guoyao
+ * @create 2019/11/25
+ */
+
+//百万富翁问题
+public class Test1125 {
+    public static void main(String[] args) {
+        System.out.println("百万富翁交出的钱：");
+        System.out.print(10*30+" ");
+        System.out.println("陌生人交出的钱：");
+          int num=(int)Math.pow(2,30)-1;
+        System.out.println(num);
+    }
+}
