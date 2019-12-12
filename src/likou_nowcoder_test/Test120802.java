59b81332142082c9afdc92e384064f671bc85b58
Nancy-06 <1456387238@qq.com>
2019-12-12 22:23:35 +0800

    年会抽奖

---
+++ src/likou_nowcoder_test/Test120802.java
@@ -0,0 +1,20 @@
+package likou_nowcoder_test;
+
+import java.util.Scanner;
+
+/**
+ * @author guoyao
+ * @create 2019/12/8
+ */
+
+//年会抽奖
+public class Test120802 {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        while(scanner.hasNextInt()){
+            int num=scanner.nextInt();
+
+        }
+    }
+}
