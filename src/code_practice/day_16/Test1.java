00c0f0a42a7b0b8a9c6a54764291c4b381860c2b
Nancy-06 <1456387238@qq.com>
2020-05-06 22:04:10 +0800

    1—n整数中1出现的此时

---
+++ src/code_practice/day_16/Test1.java
@@ -0,0 +1,32 @@
+package code_practice.day_16;
+
+/**
+ * @author guoyao
+ * @create 2020/5/5
+ */
+//1-n整数中1出现的此时
+public class Test1 {
+    public int countDigitOne(int n){
+
+        //求每个位的数字所用
+        int index=1;
+        int count=0;
+        int high=n,cur=0,low=0;
+        while(high>0){
+            high/=10;
+            cur=(n/index)%10;
+            low=n-(n/index)*index;
+            if(cur==0){
+                count+=high*index;
+            }
+            if(cur==1){
+                count+=high*index+low+1;
+            }
+            if(cur>1){
+                count+=(high+1)*index;
+            }
+            index*=10;
+        }
+        return count;
+    }
+}
