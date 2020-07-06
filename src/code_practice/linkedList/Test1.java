f4a79241086be21f2e6df63670aa561a02a300dc
Nancy-06 <1456387238@qq.com>
2020-07-06 23:58:00 +0800

    力扣题

--- src/code_practice/linkedList/Test1.java
+++ src/code_practice/linkedList/Test1.java
@@ -41,4 +41,42 @@ public class Test1 {
         return length;
     }
 
+    //链表相交
+    //他们不一定有相交的点
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB){
+        if(headA == null || headB == null){
+            return null;
+        }
+        // 走到最后一个节点，统计长度，最后一个节点相同则有相交的点，不同则没有
+        ListNode p = headA;
+        ListNode q = headB;
+        int lengthA = 1;
+        int lengthB = 1;
+        while(p.next != null){
+            lengthA ++;
+            p = p.next;
+        }
+        while(q.next != null){
+            lengthB ++;
+            q = q.next;
+        }
+        if(p == q){
+            int length = Math.abs(lengthA - lengthB);
+            for(int i = 0; i < length; i++){
+                if(lengthA > lengthB){
+                    headA = headA.next;
+                }else{
+                    headB = headB.next;
+                }
+            }
+            while(headA != null && headB != null){
+                if(headA == headB){
+                    return headA;
+                }
+                headA = headA.next;
+                headB = headB.next;
+            }
+        }
+        return null;
+    }
 }
